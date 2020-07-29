1794c49988957a92c112a21d0fb80e25541444d2
Ward Jans <ward.jans@wja-consulting.be>
2020-07-29 13:53:16 +0200

    Setup dummy project

---
+++ src/main/java/be/wja/ghactionstestartifact/GhActionsTestArtifactApplication.java
@@ -0,0 +1,13 @@
+package be.wja.ghactionstestartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhActionsTestArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhActionsTestArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/wja/ghactionstestartifact/GhActionsTestArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package be.wja.ghactionstestartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhActionsTestArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
